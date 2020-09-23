73af9758f8bf70aa094c7687916a7984950470a2
bmh1211 <64722559+bmh1211@users.noreply.github.com>
2020-09-23 09:10:27 +0900

    20200923

--- schema.sql
+++ schema.sql
@@ -3,7 +3,8 @@ CREATE TABLE IF NOT EXISTS modbus_info (
   slave INTEGER NOT NULL,
   function_code INTEGER NOT NULL,
   map_address INTEGER NOT NULL,
-  unit VARCHAR(50)
+  unit VARCHAR(50),
+  UNIQUE(point_id)
 );
 
 CREATE TABLE IF NOT EXISTS history (
@@ -19,5 +20,12 @@ CREATE TABLE IF NOT EXISTS calc_history (
   date DATE NOT NULL,
   value INTEGER NOT NULL,
   PRIMARY KEY(point_id, date),
+  FOREIGN KEY(point_id) REFERENCES modbus_info(point_id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS id_to_name (
+  point_id INTEGER ,
+  dev_name varchar(20) NOT NULL,
+  PRIMARY KEY(point_id,dev_name),
   FOREIGN KEY(point_id) REFERENCES modbus_info(point_id) ON DELETE CASCADE
 );
